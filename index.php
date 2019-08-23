618099762f3707f63c631f8dd10a34b23408189e
Payture <Support@payture.com>
2019-08-23 16:39:58 +0300

    Updating parameters for initialization
    Updating parameters for initialization: "Amount", "Url", "OrderId", "Product" and "Description"

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 require 'payture/autoload.php';
 
 // Example
-$orderId = rand();
-$amount = 1200;
+$orderId = GenerateGuid();
+$min = 1000;
+$max = 9999;
+$amount = rand( $min, $max );
 
 
-PaytureConfiguration::setMerchantKey("MerchantSbis");
+PaytureConfiguration::setMerchantKey("Merchant");
 
 //ENV_PRODUCTION - для боевой среды
 //ENV_DEVELOPMENT - для тестовой среды
@@ -18,7 +20,10 @@ $initResult = PaytureInPay::Init([
     "SessionType" => "Pay",
     "OrderId" => $orderId,
     "Amount" => $amount,
-    "IP" => $_SERVER["REMOTE_ADDR"]
+    "Product" => "Ticket",
+    "Description" => "MyTestTransaction",
+    "IP" => $_SERVER["REMOTE_ADDR"],
+    "Url" => urlencode("https://payture.com/result?orderid={orderid}&result={success}")
 ]);
 
 echo "Init result: ".print_r($initResult, true)."<br><br>";
@@ -26,4 +31,11 @@ echo "Init result: ".print_r($initResult, true)."<br><br>";
 
 if ($initResult->Success) {
     echo "Generated Link: ".PaytureInPay::generatePayLink()."<br><br>";
+}
+
+
+function GenerateGuid()
+{
+    if (function_exists('com_create_guid') === true) { return trim(com_create_guid(), '{}'); }
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 }
